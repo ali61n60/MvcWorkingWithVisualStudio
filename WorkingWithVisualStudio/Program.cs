8e74c7dbcb89ecb82b2749c2c5f846e2b2da096e
Ali <ali62n62@yahoo.com>
2020-04-26 02:07:19 +0430

    7/2/99

--- WorkingWithVisualStudio/Program.cs
+++ WorkingWithVisualStudio/Program.cs
@@ -14,12 +14,17 @@ namespace WorkingWithVisualStudio
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
+
+            host.Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+       
     }
 }
