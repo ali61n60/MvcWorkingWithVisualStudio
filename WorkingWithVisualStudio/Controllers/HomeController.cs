a7bbb7e050b3ce4282159264a363c5ef252baf20
ali <ali@ALI-PC>
2020-04-30 22:22:41 +0430

    addproduct view added

--- WorkingWithVisualStudio/Controllers/HomeController.cs
+++ WorkingWithVisualStudio/Controllers/HomeController.cs
@@ -11,11 +11,21 @@ namespace WorkingWithVisualStudio.Controllers
 {
     public class HomeController : Controller
     {
+        SimpleRepository Repository = SimpleRepository.SharedRepository;
 
-        public IActionResult Index()
+        public IActionResult Index() => View(Repository.Products
+        .Where(p => p?.Price < 50));
+
+        [HttpGet]
+        public IActionResult AddProduct() => View(new Product());
+
+        [HttpPost]
+        public IActionResult AddProduct(Product p)
         {
-            return View(SimpleRepository.SharedRepository.Products.Where(p => p?.Price < 50));
-        }
+            Repository.AddProduct(p);
+            return RedirectToAction("Index");
 
+
+        }
     }
 }
