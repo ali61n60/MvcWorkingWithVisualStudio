5479b8f5892dd4cceba94f6e03767bf424241efa
ali <ali@ALI-PC>
2020-04-30 22:08:46 +0430

    finished on laptop

--- WorkingWithVisualStudio/wwwroot/js/bundle.js
+++ WorkingWithVisualStudio/wwwroot/js/bundle.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     var element = document.createElement("p");
-    element.textContent = "This is the element from the third.js file";
+    element.textContent = "This is the element from the (modified) third.js file";
     document.querySelector("body").appendChild(element);
 });
 document.addEventListener("DOMContentLoaded", function () {
